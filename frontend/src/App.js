f25203451e4a67aea0f446c1cbf1da23eb8e3cfc
jpagcal <jpagcal_@outlook.com>
2025-09-13 17:55:07 -0600

    login ui page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import JobApplications from './pages/JobApplications';
 import ResumeManager from './pages/ResumeManager';
 import NotificationsPage from './pages/NotificationsPage'; 
 import ResponsesPage from './pages/ResponsesPage'; 
+import Login from './pages/Login';
 
 /**
  * App Component
@@ -34,8 +35,10 @@ function App() {
           {/* Main content container */}
           <div className="container">
             <Routes>
+              <Route path="/" element={<Login />} />
+
               {/* Dashboard route - shows overview and statistics */}
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
               
               {/* Job applications route - manage job applications */}
               <Route path="/applications" element={<JobApplications />} />
@@ -56,3 +59,36 @@ function App() {
 }
 
 export default App;
+
+
+//function AppLayout() {
+//  const location = useLocation();
+//
+//  return (
+//    <div className="App">
+//      {/* Only show Navbar if not on "/" */}
+//      {location.pathname !== "/" && <Navbar />}
+//      
+//      <div className="container">
+//        <Routes>
+//          <Route path="/" element={<Login />} />
+//          <Route path="/dashboard" element={<Dashboard />} />
+//          <Route path="/applications" element={<JobApplications />} />
+//          <Route path="/resumes" element={<ResumeManager />} />
+//          <Route path="/notifications" element={<NotificationsPage />} />
+//          <Route path="/responses" element={<ResponsesPage />} />
+//        </Routes>
+//      </div>
+//    </div>
+//  );
+//}
+//
+//function App() {
+//  return (
+//    <NotificationProvider>
+//      <Router>
+//        <AppLayout />
+//      </Router>
+//    </NotificationProvider>
+//  );
+//}//////////////////////////////////////////////////////////////
