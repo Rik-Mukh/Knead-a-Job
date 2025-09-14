ad33cf8a5a20091b4e3711e360aa37fd678cf015
jpagcal <jpagcal_@outlook.com>
2025-09-14 14:16:56 -0600

    ui: remove navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { NotificationProvider } from './contexts/NotificationContext';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
@@ -15,9 +15,6 @@ import ResumeManager from './pages/ResumeManager';
 import NotificationsPage from './pages/NotificationsPage'; 
 import ResponsesPage from './pages/ResponsesPage'; 
 import Login from './pages/Login';
-import ResumeTemplate from './pages/ResumeTemplate';
-import GmailMessages from './components/GmailMessages'
-import MatchAnalysis from './pages/MatchAnalysis';
 
 /**
  * App Component
@@ -27,76 +24,36 @@ import MatchAnalysis from './pages/MatchAnalysis';
  * 
  * @returns {JSX.Element} The main app component
  */
+function AppLayout() {
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      {/* Only show Navbar if not on "/" */}
+      {location.pathname !== "/" && <Navbar />}
+      
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/applications" element={<JobApplications />} />
+          <Route path="/resumes" element={<ResumeManager />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+          <Route path="/responses" element={<ResponsesPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <NotificationProvider>
       <Router>
-        <div className="App">
-          {/* Navigation bar component */}
-          <Navbar />
-          
-          {/* Main content container */}
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Login />} />
-
-              {/* Dashboard route - shows overview and statistics */}
-              <Route path="/dashboard" element={<Dashboard />} />
-              
-              {/* Job applications route - manage job applications */}
-              <Route path="/applications" element={<JobApplications />} />
-              
-              {/* Resume template route - manage structured resume data */}
-              <Route path="/resume-template" element={<ResumeTemplate />} />
-              
-              {/* Match analysis route - analyze resume match scores */}
-              <Route path="/match-analysis" element={<MatchAnalysis />} />
-              
-              {/* Notifications route */}
-              <Route path="/notifications" element={<NotificationsPage />} />
-              
-              <Route path="/responses" element={<ResponsesPage />} />
-
-              <Route path="/gmail" element={<GmailMessages />} /> 
-            </Routes>
-          </div>
-        </div>
+        <AppLayout />
       </Router>
     </NotificationProvider>
   );
 }
 
 export default App;
-
-
-//function AppLayout() {
-//  const location = useLocation();
-//
-//  return (
-//    <div className="App">
-//      {/* Only show Navbar if not on "/" */}
-//      {location.pathname !== "/" && <Navbar />}
-//      
-//      <div className="container">
-//        <Routes>
-//          <Route path="/" element={<Login />} />
-//          <Route path="/dashboard" element={<Dashboard />} />
-//          <Route path="/applications" element={<JobApplications />} />
-//          <Route path="/resumes" element={<ResumeManager />} />
-//          <Route path="/notifications" element={<NotificationsPage />} />
-//          <Route path="/responses" element={<ResponsesPage />} />
-//        </Routes>
-//      </div>
-//    </div>
-//  );
-//}
-//
-//function App() {
-//  return (
-//    <NotificationProvider>
-//      <Router>
-//        <AppLayout />
-//      </Router>
-//    </NotificationProvider>
-//  );
-//}//////////////////////////////////////////////////////////////
